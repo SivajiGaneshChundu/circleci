81f0cc48f0c0bd252817c66274dda64b070c6880
SIVAJI GANESH <schundu@dynamite.ca>
2020-06-15 22:45:32 -0400

    changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+require('dotenv').config()
+
+
 exports.hello = () => {
     console.log('hello')
+    const digital = process.env.DIGITAL_SFTP_HOST
+    const digi = process.env.DIGITAL_SFTP_PRIVATE_KEY
+    console.log(digital, digi)
 }
